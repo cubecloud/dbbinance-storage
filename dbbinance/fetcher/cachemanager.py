30932b6ea1f1766ed8029942bbf5456edf965fda
cubecloud <rainmaverick@mail.ru>
2024-08-07 09:14:36 +0300

    + add mp RLock support

--- dbbinance/fetcher/cachemanager.py
+++ dbbinance/fetcher/cachemanager.py
@@ -2,15 +2,16 @@ import sys
 from typing import Union
 from collections import OrderedDict
 from mlthread_tools import mlt_mutex
+from multiprocessing import RLock as mlp_mutex
 import logging
 
-__version__ = 0.011
+__version__ = 0.015
 
 logger = logging.getLogger()
 
 
 class CacheManager:
-    def __init__(self, max_memory_gb: Union[float, int] = 3):
+    def __init__(self, max_memory_gb: Union[float, int] = 3, mutex: str = 'mlt'):
         """
         Initialize the Cache class with an optional maximum memory limit in gigabytes.
 
@@ -24,13 +25,19 @@ class CacheManager:
         self.__cache = OrderedDict()
         self.max_memory_bytes = int(max_memory_gb * 1024 * 1024 * 1024)  # Convert max_memory_gb to bytes
         self.current_memory_usage = 0
+        if mutex == 'mlt':
+            self.lock = mlt_mutex
+        elif mutex == 'mp':
+            self.lock = mlp_mutex
+        else:
+            sys.exit(f'Error: Unknown option {mutex}')
 
     @property
     def cache(self):
         return self.__cache
 
     def update_cache(self, key, value):
-        with mlt_mutex:
+        with self.lock:
             value_size = sys.getsizeof(value)
             if value_size > self.max_memory_bytes:
                 logger.warning(f"{self.__class__.__name__}: "
@@ -42,7 +49,7 @@ class CacheManager:
             self.current_memory_usage = sum(sys.getsizeof(v) for v in self.__cache.values())
 
     def popitem(self, last=False):
-        with mlt_mutex:
+        with self.lock:
             item = self.__cache.popitem(last=last)
             self.current_memory_usage = sum(sys.getsizeof(v) for v in self.__cache.values())
         return item
