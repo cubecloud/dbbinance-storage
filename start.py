be057c4599597f475ed07f724e1d8754206d6153
cubecloud <rainmaverick@mail.ru>
2025-06-20 19:08:51 +0300

    + testing logger PYTHONUNBUFFERED

--- start.py
+++ start.py
@@ -5,20 +5,14 @@ from dbbinance.config import ConfigBinance
 from dbbinance.config import ConfigPostgreSQL
 from dbbinance.fetcher.datafetcher import DataUpdater
 from dbbinance.config.dockerized import is_pythonunbuffered
-from multiprocessing import get_logger
 
 version = 0.80
 
-logger = get_logger()
+logger = logging.getLogger()
 
 logger.setLevel(logging.INFO)
 
-file_handler = TimedRotatingFileHandler('dbdata_updater.log', when='D', interval=3, backupCount=3)
-file_handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(processName)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(formatter)
