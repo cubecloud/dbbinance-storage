48105db238b187ff3aefbec2d82d0bedd3c23311
cubecloud <rainmaverick@mail.ru>
2025-06-21 19:23:48 +0300

    ~ bug fixes for docker

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-cat *.env > temp && sudo docker run --cpu-shares 128 --env-file=temp -e PSGSQL_HOST_IP=host.docker.internal --name dbupdater dbbinance-image && rm temp
+cat *.env > temp && sudo docker run --cpu-shares 128 --env-file=temp --net=host -e PSGSQL_HOST_IP=192.168.1.145 --name dbupdater dbbinance-image && rm temp
